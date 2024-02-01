7d2a6161d8907724aabec01855f2d4d9b138edfe
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2024-01-31 16:49:04 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 /* global process */
 
 const express = require('express');
-const path = require('path');
 const app = express();
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Define routes
 app.get('/', (req, res) => {
@@ -41,17 +37,13 @@ app.get('/foo', (req, res) => {
     res.send('and sometimes that');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dog-breeds.html'));
-});
-
 // Handle 404
 app.use((req, res) => {
     res.status(404).send('404 - Not Found');
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is successfully running on port ${PORT}`);
 });
